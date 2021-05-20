4bb54d5f16d26e7480650c9bd04259ec84569fac
Lincoln Martinez <lincoln.martinez@tecsup.edu.pe>
2021-05-20 11:32:34 -0500

    Deploy

--- src/main/java/com/tutorial/cloudinaryrest/CloudinaryrestApplication.java
+++ src/main/java/com/tutorial/cloudinaryrest/CloudinaryrestApplication.java
@@ -2,9 +2,10 @@ package com.tutorial.cloudinaryrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CloudinaryrestApplication {
+public class CloudinaryrestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudinaryrestApplication.class, args);
